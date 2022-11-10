3b5a78f14f2cad84ea059975b84b9a386f13fcaa
Bitm-m <77094782+Bitm-m@users.noreply.github.com>
2022-11-10 20:40:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget https://github.com/libsgh/PanIndex/releases/download/v3.0.8/PanIndex-linux-amd64.tar.gz
+wget https://github.com/libsgh/PanIndex/releases/download/v3.1.0/PanIndex-linux-amd64.tar.gz
 tar -zxvf PanIndex-linux-amd64.tar.gz
 
 # 赋予执行权限
